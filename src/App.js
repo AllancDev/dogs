2d793da1fddda6ecde8ffddbb9879eb87bfbea79
Allan Cristian <Allan.cristian@outlook.com.br>
2021-01-16 21:11:23 -0300

    add css modules header and footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-import Api from './api/Api';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login/Login';
 
 function App() {
-  return <Api />;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
